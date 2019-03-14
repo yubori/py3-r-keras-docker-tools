1594094f89cf145de712467d312788971df4b7d4
Yu Enokibori <yubori>
2019-03-14 02:51:41 +0000

    Fix a bug of making links to .local/bin

--- setup.sh
+++ setup.sh
@@ -6,7 +6,7 @@ SCRIPT_DIR=$(dirname $SCRIPT_FULL_PATH)
 echo "mkdir -p $LOCAL_BIN"
 mkdir -p "$LOCAL_BIN"
 
-for sh_file in ${SCRIPT_DIR}/local_bin;
+for sh_file in $(ls ${SCRIPT_DIR}/local_bin/*);
 do
 	SH_FIKE_FULLPATH=$(readlink -f $sh_file)
 	echo "ln -s $SH_FIKE_FULLPATH $LOCAL_BIN"
@@ -15,9 +15,15 @@ do
 	chmod +x "$SH_FIKE_FULLPATH"
 done
 
+cat<<EOF
 ln -s "${SCRIPT_DIR}/local_bin/run.r.sh ${LOCAL_BIN}/run.r"
 ln -s "${SCRIPT_DIR}/local_bin/run.py.sh ${LOCAL_BIN}/run.py"
 ln -s "${SCRIPT_DIR}/local_bin/run.bash.sh ${LOCAL_BIN}/run.bash"
+EOF
+
+ln -s "${SCRIPT_DIR}/local_bin/run.r.sh" "${LOCAL_BIN}/run.r"
+ln -s "${SCRIPT_DIR}/local_bin/run.py.sh" "${LOCAL_BIN}/run.py"
+ln -s "${SCRIPT_DIR}/local_bin/run.bash.sh" "${LOCAL_BIN}/run.bash"
 
 DOCKER_CNT=$(cat $SCRIPT_DIR/local_bin/docker_cnt.txt)
 sudo docker build dockers/tf-gpu-py3-r-keras -t $DOCKER_CNT
