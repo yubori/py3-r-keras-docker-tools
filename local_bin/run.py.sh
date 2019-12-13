914c189074566cbf9b8794777c1587d7aec9cfea
Yu Enokibori <yubori>
2019-12-13 02:47:06 +0000

    Change tf version from 1 to 2

--- local_bin/run.py.sh
+++ local_bin/run.py.sh
@@ -3,10 +3,11 @@ SCRIPT_FULL_PATH=$(readlink -f $0)
 SCRIPT_DIR=$(dirname $SCRIPT_FULL_PATH)
 . $SCRIPT_DIR/common.sh
 
-while : 
+while true
 do
     docker run $DOC_OPT $DOCKER_CNT sh -c "python3 $*"
-    if [ if $LOOP -ne 1 -o $? -eq 0 ]; then
+    RES=$?
+    if [ $LOOP -ne 1 -o $RES -eq 0 ]; then
        break 
     fi
 done
