29d80aa283b3be0355b4d2e675efe9767d8ed213
yubori <vori003@gmail.com>
2020-03-18 02:44:46 +0000

    Change "while :" to "while true"

--- local_bin/run.py.sh
+++ local_bin/run.py.sh
@@ -3,10 +3,12 @@ SCRIPT_FULL_PATH=$(readlink -f $0)
 SCRIPT_DIR=$(dirname $SCRIPT_FULL_PATH)
 . $SCRIPT_DIR/common.sh
 
-while : 
+while true
 do
     docker run $DOC_OPT $DOCKER_CNT sh -c "python3 $*"
-    if [ if $LOOP -ne 1 -o $? -eq 0 ]; then
+    RES=$?
+    #echo "$RES"
+    if [ $RES -eq 0 -o $LOOP -ne 1 ]; then
        break 
     fi
 done
